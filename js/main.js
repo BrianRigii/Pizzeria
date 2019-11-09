8c5a35ece5888dbf3c5f892d780c0e3bf5b6221f
John Brian <johnbryanjb011@gmail.com>
2019-11-09 13:19:13 +0300

    added a rest button to the form

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ function meatlover(){
    var pizzatitle= document.getElementById("pizzatitle")
    pizzatitle.innerText = "Meat Lover"
 }
+function vegetarian(params) {
+    var pizzatitle= document.getElementById("pizzatitle")
+   pizzatitle.innerText = "Vegeterian Heaven"
+    
+}
+function toni(params) {
+    var pizzatitle= document.getElementById("pizzatitle")
+   pizzatitle.innerText = "Toni Pepperoni"
+    
+}

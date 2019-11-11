ac4e6b1b2d470e05fe7dce2fa209d43198078e93
John Brian <johnbryanjb011@gmail.com>
2019-11-11 12:51:22 +0300

    changed valus to prices

--- js/main.js
+++ js/main.js
@@ -24,7 +24,9 @@ function toni() {
 
 
 
-//business logic
+
+
+
 function pizza (name ,size ,toppings,crust,qty){
   this.name= name;
   this.size=size;
@@ -36,19 +38,33 @@ function pizza (name ,size ,toppings,crust,qty){
 
 function addcart(){
   var pizzaName =pizzatitle.textContent
-  var pizzaSize =$(".size:checked").val()
-  var pizzaToppings =$(".toppings:checked").val()
-  var pizzaCrust = $(".crust:checked").val()
-  var PizzaQty= $("#quantity").val()
+  var pizzaSize =$(".size:checked").val();
+  var pizzaToppings =$(".toppings:checked").val();
+  var pizzaCrust = $(".crust:checked").val();
+  var PizzaQty= $("#quantity").val();
 
   let lepizza = new pizza (pizzaName,pizzaSize,pizzaToppings,pizzaCrust,PizzaQty)
   console.log(lepizza)
   pizzatitle.innerText = "Pick another pizza above";
 
 
-
   $(".table").append('<tr>'+'<td>'+lepizza.name+'</td>'+
   '<td>'+lepizza.size+'</td>'+'<td>'+lepizza.toppings+'</td>' + '<td>'+lepizza.crust+'</td>'
-  +'<td>'+lepizza.qty+'</td>'                        )
+  +'<td>'+lepizza.qty+'</td>'   )
+
+  // Pizza cost
+
+    var sizeCost = $(".size:checked").cost()
+    console.log(sizeCost)
   
 }
+
+function deliver(){
+  prompt("Where do you want you pizza deliverd")
+  
+}
+
+
+
+
+
